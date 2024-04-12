2438094d4527a150964661b6779a14561474544d
SollGomez <sogomez@frba.utn.edu.ar>
2024-04-12 18:28:46 -0300

    color secundario

--- src/main/java/ar/edu/utn/frba/dds/Prenda.java
+++ src/main/java/ar/edu/utn/frba/dds/Prenda.java
@@ -3,14 +3,19 @@ package ar.edu.utn.frba.dds;
 import static java.util.Objects.requireNonNull;
 
 public class Prenda {
-  TipoDePrenda tipo;
+  Categoria tipo;
   Material tela;
   Color color;
+  Color colorSecundario;
 
   public Prenda(Categoria tipo, Material tela, Color color) {
     this.tipo = requireNonNull(tipo, "El tipo de prenda es obligatorio");
     this.tela = requireNonNull(tela, "El material de la prenda es obligatorio");
     this.color = requireNonNull(color, "El color principal es obligatorio");
+  }
+
+  public void setColorSecundario(Color colorSecundario) {
+    this.colorSecundario = colorSecundario;
   }
 
 }
